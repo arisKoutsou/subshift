1275b9a69c27a5e1a6f62ad78dd4818c0320744b
arisKoutsou <gkoutsourid@hotmail.com>
2018-02-18 21:11:11 +0200

    Made a small change for testing ...

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // Created by aris on 18/02/18.
 //
 
-#include <stdlib.h>
+#include <cstdlib>
 #include <iostream>
 
 using namespace std;
@@ -10,5 +10,6 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
     cout << "This is the Subtitle Shifter!" << endl;
+    cout << "But it's still being constructed..." << endl;
     return EXIT_SUCCESS;
 }
